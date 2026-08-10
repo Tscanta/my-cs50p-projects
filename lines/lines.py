5ee0fc251d0f5ebb5ad7cb92d2fcef8e9ec1af51
Tshe <159624211+Tscanta@users.noreply.github.com>
2026-08-10 11:14:52 +0000

    Mon, Aug 10, 2026, 4:44 PM +05:30

--- lines/lines.py
+++ lines/lines.py
@@ -6,3 +6,10 @@ elif len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 
 filename = sys.argv[1]
+
+if not filename.endswith(".py"):
+    sys.exit("Not a python file")
+
+try:
+    with open
+
