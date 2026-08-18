b36f906fd84d24e4ccd57d0a5a95f3fe1373d812
Tshe <159624211+Tscanta@users.noreply.github.com>
2026-08-18 17:25:12 +0000

    Tue, Aug 18, 2026, 10:55 PM +05:30

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,3 +2,19 @@ import sys
 import csv
 
 from tabulate import tabulate
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+filename = sys.argv[1]
+
+if not filename.endswith(".csv"):
+    sys.exit("Not a CSV file")
+
+try:
+    ...
+
+except FileNotFoundError: 
+    sys.exit("File does not exist")
